192071c5a16dbeb721ead9edd6a7bb4efab66c20
He Tao <hetao@buaa.edu.cn>
2015-11-13 02:48:14 +0800

    Add two parsers.

--- pl0_parser.hpp
+++ pl0_parser.hpp
@@ -9,7 +9,7 @@
 #include "parsec.hpp"
 using namespace std;
 
-// 数字
+// <数字> ::= 0|1|2|3 ... 8|9
 struct pl0_digit {
     using parser_type = char;
     constexpr pl0_digit() {} 
@@ -19,7 +19,7 @@ struct pl0_digit {
     }
 } constexpr pl0_digit;
 
-// 字母
+// <字母> ::= a|b|c|d ... x|y|z |A|B…|Z
 struct pl0_alpha {
     using parser_type = char;
     constexpr pl0_alpha() {}
@@ -29,7 +29,7 @@ struct pl0_alpha {
     }
 } constexpr pl0_alpha;
 
-// 字符
+// <字符> ::= '<字母>' | '<数字>'
 struct pl0_char {
     using parser_type = char;
     constexpr pl0_char() {}
@@ -40,7 +40,7 @@ struct pl0_char {
     }
 } constexpr pl0_char;
 
-// 字符串
+// <字符串> ::= "{十进制编码为32,33,35-126的ASCII字符}"
 struct pl0_charseq {
     using parser_type = string;
     constexpr pl0_charseq() {}
@@ -54,7 +54,7 @@ struct pl0_charseq {
     }
 } constexpr pl0_charseq;
 
-// 无符号整数
+// <无符号整数> ::= <数字>{<数字>}
 struct pl0_unsigned {
     using parser_type = unsigned int;
     constexpr pl0_unsigned() {}
@@ -68,7 +68,7 @@ struct pl0_unsigned {
     }
 } constexpr pl0_unsigned;
 
-// 常量
+// <常量> ::= [+| ]<无符号整数>|<字符>
 struct pl0_const {
     using parser_type = int;
     constexpr pl0_const() {}
@@ -92,10 +92,11 @@ struct pl0_const {
     }
 } constexpr pl0_const;
 
-// 标识符
+// <标识符> ::= <字母>{<字母>|<数字>}
 struct pl0_identify {
     using parser_type = string;
     constexpr pl0_identify() {}
+    string name() const { return "pl0 identify"; }
     pair<int, pair<parser_type, string>> operator () (string const & text) const {
         auto parser = pl0_alpha + (++(pl0_alpha | pl0_digit));
         function<parser_type (pair<char, vector<char>>)> fn = [](pair<char, vector<char>> const & chs) {
@@ -105,28 +106,71 @@ struct pl0_identify {
     }
 } constexpr pl0_identify;
 
-// 基本类型
+// <基本类型> ::= integer | char
 struct pl0_primitive_type {
     using parser_type = string;
     constexpr pl0_primitive_type() {}
+    string name() const { return "pl0 primitive type"; }
     pair<int, pair<parser_type, string>> operator () (string const & text) const {
         auto parser = string_literal("integer") | string_literal("char");
         return (spaces >> parser << spaces)(text);
     }
 } constexpr pl0_primitive_type;
 
-// 类型
+// <类型> ::= <基本类型>|array'['<无符号整数>']' of <基本类型>
 struct pl0_type {
     using parser_type = pair<string, int>; // <type, dimension>
     constexpr pl0_type() {}
+    string name() const { return "pl0 type"; }
     pair<int, pair<parser_type, string>> operator () (string const & text) const {
         function<parser_type (string)> fn = [](string const & t) { return make_pair(t, -1); };
         function<parser_type (pair<unsigned int, string>)> flip = [](pair<unsigned int, string> arg) { return make_pair(arg.second, arg.first); };
         auto parser = (pl0_primitive_type / fn) // not array
             | ((string_literal("array") >> spaces >> (character('[') >> pl0_unsigned << character(']')) << spaces << string_literal("of")) + pl0_primitive_type) / flip; // array
-        return parser(text);
+        return (spaces >> parser << spaces)(text);
     }
 } constexpr pl0_type;
 
+// <加法运算符> ::= +|-
+struct pl0_addop {
+    using parser_type = char;
+    constexpr pl0_addop() {}
+    string name() const { return "pl0 addop"; }
+    pair<int, pair<parser_type, string>> operator () (string const & text) const {
+        return (spaces >> (character('+') | character('-')) << spaces)(text);
+    }
+} constexpr pl0_addop;
+
+// <乘法运算符> ::= *|/
+struct pl0_multop {
+    using parser_type = char;
+    constexpr pl0_multop() {}
+    string name() const { return "pl0 multop"; }
+    pair<int, pair<parser_type, string>> operator () (string const & text) const {
+        return (spaces >> (character('*') | character('/')) << spaces)(text);
+    }
+} constexpr pl0_multop;
+
+// <变量说明> ::= <标识符>{, <标识符>} : <类型>
+struct pl0_vardesc {
+    using parser_type = pair<vector<string>, pl0_type::parser_type>; // <identifies, type>
+    constexpr pl0_vardesc() {}
+    string name() const { return "pl0 var description"; }
+    pair<int, pair<parser_type, string>> operator () (string const & text) const {
+        auto parser = (pl0_identify % character(',')) + (character(':') >> pl0_type);
+        return (spaces >> parser << spaces)(text);
+    }
+} constexpr pl0_vardesc;
+
+// <变量说明部分> ::= var <变量说明> ; {<变量说明>;}
+struct pl0_vardesc_stmt {
+    using parser_type = vector<pl0_vardesc::parser_type>;
+    constexpr pl0_vardesc_stmt() {}
+    string name() const { return "pl0 var description statement."; }
+    pair<int, pair<parser_type, string>> operator () (string const & text) const {
+        auto parser = string_literal("var") >> ((pl0_vardesc << character(';'))++);
+        return (spaces >> parser << spaces)(text);
+    }
+} constexpr pl0_vardesc_stmt;
 
 #endif /* __PLO_PARSER_HPP__ */
