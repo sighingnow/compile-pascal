b2fce612d2975adefd18ec9b1f1d24b3c2973c71
He Tao <hetao@buaa.edu.cn>
2015-12-21 17:02:23 +0800

    Add a new test case.

---
+++ pl0_cases/test_ref3.pas
@@ -0,0 +1,19 @@
+const x=20;
+var y, z, a: integer;
+
+function xel(var arg1, arg2: integer): integer;
+begin
+    z := 2;
+	arg1 := arg1 + arg2 + x
+end;
+
+begin
+    y := 10;
+    z := 9;
+    a := xel(y, z);
+    if y = 32 then
+        write("reference argument 3 success.")
+    else
+        write("reference argument 3 failed.", a)
+    ;
+end.
